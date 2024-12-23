cecc9f30af162bcb886ae72798fc2a660faa7cea
hakata-jpn <blandingren425@gmail.com>
2024-12-23 10:48:31 -0600

    reformatted plugins to folder

--- init.lua
+++ init.lua
@@ -27,51 +27,8 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+
 -- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-        {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-}
+require("lazy").setup("plugins")
 
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "catppuccin" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true }, }
-})
 
--- ADDITIONAL CONFIGS, PLUG YOUR SHIT HERE
-
--- Neotree Config
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-vim.keymap.set('n', '<C-b>', ':Neotree filesystem close<CR>') 
-
--- Treesitter Config
-local config = require("nvim-treesitter.configs")
-config.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
--- Color Scheme: catppuccin
-vim.cmd.colorscheme "catppuccin"
-
--- Telescope Config
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
