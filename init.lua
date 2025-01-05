78266b7d66faa61db7cf6f8bc19a609ed0a43559
Hakata-jpn <blandingren425@gmail.com>
2025-01-05 22:32:04 +0000

    fixed clipboard not working in SSH

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.opt.clipboard="unnamedplus"
+
 
 -- adds OSC Support to all terminals 
 local function copy(lines, _)
