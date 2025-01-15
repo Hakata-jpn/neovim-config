45b2c1e67461d488fbccf5eb62856f911c9ffd66
hakata-jpn <blandingren425@gmail.com>
2025-01-15 13:34:30 -0600

    added support for LSPs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "astro", "cssmodules_ls", "ts_ls", "docker_compose_language_service", "yamlls"}
       })
     end
   },
@@ -18,7 +18,15 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.astro.setup({})
+      lspconfig.cssmodules_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.docker_compose_language_service.setup({})
+      lspconfig.yamlls.setup({})
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   },
 }
