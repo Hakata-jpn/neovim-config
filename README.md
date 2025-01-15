43a1b9f68089f27ac04e1fbf827d673b3bd99b6d
hakata-jpn <blandingren425@gmail.com>
2025-01-15 16:34:47 -0600

    updated README, minor changes to config

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Neovim Config Documentation
 
-> TBD, need to add more to it first
+<!--toc:start-->
+
+- [Neovim Config Documentation](#neovim-config-documentation)
+  - [Dependencies:](#dependencies)
+  - [Process:](#process)
+  <!--toc:end-->
+
+## Dependencies
+
+- Nerd Font
+- npm
+- python3 and pip
+  - use `sudo apt install python3.12-venv` to ensure pip is properly installed
+- Linters (install with `:Mason`)
+  - eslint_d
+  - markdownlint
+  - stylelint
+  - yamllint
+- Formatter (install with `:Mason`)
+  - markdownlint
+  - prettier
+  - stylua
+
+## Process
+
+1. Install `npm` `python3` and `python3.12-venv` on the local machine
+2. Pull from GitHub Repo
+3. Initialize Neovim
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,41 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "astro", "cssmodules_ls", "ts_ls", "docker_compose_language_service", "yamlls", "marksman"}
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.astro.setup({})
-      lspconfig.cssmodules_ls.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.docker_compose_language_service.setup({})
-      lspconfig.yamlls.setup({})
-      lspconfig.marksman.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"astro",
+					"cssmodules_ls",
+					"ts_ls",
+					"docker_compose_language_service",
+					"yamlls",
+					"marksman",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.astro.setup({})
+			lspconfig.cssmodules_ls.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.docker_compose_language_service.setup({})
+			lspconfig.yamlls.setup({})
+			lspconfig.marksman.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
